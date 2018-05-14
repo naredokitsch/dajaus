ab07e153c38757e2d36a8cfb68e53b9f741a597c
naredokitsch <naredokitsch@utexas.edu>
2018-05-14 17:50:35 -0500

    BigTable texturized

--- figura_class.cpp
+++ figura_class.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-#include "../texture.cpp"
+#include "texture.cpp"
 
 using namespace std;
 
@@ -76,76 +76,56 @@ class figura {
 		}
 
 		void solid () {
-
 			int i = 0;
 			if(q[0] > 0) {
-				
+				for (i = 10 ; i <= q[0] ;  i+=12) {
 					glBegin(GL_QUADS);
-					for (i = 1 ; i <= q[0] ;  i+=3) {
+						glNormal3f(n[(q[i - 7] * 3) - 2] , n[(q[i - 7] * 3) - 1] , n[q[i - 7] * 3]) ; glVertex3f(c[(q[i - 9] * 3) - 2] , c[(q[i - 9] * 3) - 1] , c[q[i - 9] * 3]);
+						glNormal3f(n[(q[i - 4] * 3) - 2] , n[(q[i - 4] * 3) - 1] , n[q[i - 4] * 3]) ; glVertex3f(c[(q[i - 6] * 3) - 2] , c[(q[i - 6] * 3) - 1] , c[q[i - 6] * 3]);
+						glNormal3f(n[(q[i - 1] * 3) - 2] , n[(q[i - 1] * 3) - 1] , n[q[i - 1] * 3]) ; glVertex3f(c[(q[i - 3] * 3) - 2] , c[(q[i - 3] * 3) - 1] , c[q[i - 3] * 3]);
 						glNormal3f(n[(q[i + 2] * 3) - 2] , n[(q[i + 2] * 3) - 1] , n[q[i + 2] * 3]) ; glVertex3f(c[(q[i] * 3) - 2] , c[(q[i] * 3) - 1] , c[q[i] * 3]);
-					}
 					glEnd();
-				
-			}
-
-			if(t[0] > 0) {
-				
-					glBegin(GL_TRIANGLES);
-					for (i = 1 ; i <= t[0] ;  i+=3) {
-						glNormal3f(n[(t[i + 2] * 3) - 2] , n[(t[i + 2] * 3) - 1] , n[t[i + 2] * 3]) ; glVertex3f(c[(t[i] * 3) - 2] , c[(t[i] * 3) - 1] , c[t[i] * 3]);
-					}
-					glEnd();
-				
-			}
-		}
-
-
-		void texturized() {
-			int i = 0;
-			if(q[0] > 0) {
-					glBindTexture(GL_TEXTURE_2D, textura.GLindex);
-					glBegin(GL_QUADS);
-					for (i = 1 ; i <= q[0] ;  i+=3) {
-						glTexCoord2f(x[(q[i + 1] * 2) - 1] , x[q[i + 1] * 2]) ; glNormal3f(n[(q[i + 2] * 3) - 2] , n[(q[i + 2] * 3) - 1] , n[q[i + 2] * 3]) ; glVertex3f(c[(q[i] * 3) - 2] , c[(q[i] * 3) - 1] , c[q[i] * 3]);
-					}
-					glEnd();
-				
-			}
-
-			if(t[0] > 0) {
-					glBindTexture(GL_TEXTURE_2D, textura.GLindex);
-					glBegin(GL_TRIANGLES);
-					for (i = 1 ; i <= t[0] ;  i+=3) {
-						glTexCoord2f(x[(t[i + 1] * 2) - 1] , x[t[i + 1] * 2]) ; glNormal3f(n[(t[i + 2] * 3) - 2] , n[(t[i + 2] * 3) - 1] , n[t[i + 2] * 3]) ; glVertex3f(c[(t[i] * 3) - 2] , c[(t[i] * 3) - 1] , c[t[i] * 3]);
-					}
-					glEnd();
-				
-			}
-		}
-
-/*
-		void forced_texturized () {
-			int i = 0;
-			if(q[0] > 0) {
-					glBegin(GL_QUADS);
-					for (i = 10 ; i <= q[0] ;  i+=12) {
-						glTexCoord2f(0.0 , 0.0) ; glNormal3f(n[(q[i - 7] * 3) - 2] , n[(q[i - 7] * 3) - 1] , n[q[i - 7] * 3]) ; glVertex3f(c[(q[i - 9] * 3) - 2] , c[(q[i - 9] * 3) - 1] , c[q[i - 9] * 3]);
-						glTexCoord2f(0.0 , 0.1) ; glNormal3f(n[(q[i - 4] * 3) - 2] , n[(q[i - 4] * 3) - 1] , n[q[i - 4] * 3]) ; glVertex3f(c[(q[i - 6] * 3) - 2] , c[(q[i - 6] * 3) - 1] , c[q[i - 6] * 3]);
-						glTexCoord2f(1.0 , 1.0) ; glNormal3f(n[(q[i - 1] * 3) - 2] , n[(q[i - 1] * 3) - 1] , n[q[i - 1] * 3]) ; glVertex3f(c[(q[i - 3] * 3) - 2] , c[(q[i - 3] * 3) - 1] , c[q[i - 3] * 3]);
-						glTexCoord2f(1.1 , 1.1) ; glNormal3f(n[(q[i + 2] * 3) - 2] , n[(q[i + 2] * 3) - 1] , n[q[i + 2] * 3]) ; glVertex3f(c[(q[i] * 3) - 2] , c[(q[i] * 3) - 1] , c[q[i] * 3]);		
-					glEnd();
+				}
 			}
 
 			if(t[0] > 0) {
 				for (i = 7 ; i <= t[0] ;  i+=9) {
 					glBegin(GL_TRIANGLES);
-						glTexCoord2f(0.0 , 0.0) ; glNormal3f(n[(t[i - 4] * 3) - 2] , n[(t[i - 4] * 3) - 1] , n[t[i - 4] * 3]) ; glVertex3f(c[(t[i - 6] * 3) - 2] , c[(t[i - 6] * 3) - 1] , c[t[i - 6] * 3]);
-						glTexCoord2f(0.0 , 0.1) ; glNormal3f(n[(t[i - 1] * 3) - 2] , n[(t[i - 1] * 3) - 1] , n[t[i - 1] * 3]) ; glVertex3f(c[(t[i - 3] * 3) - 2] , c[(t[i - 3] * 3) - 1] , c[t[i - 3] * 3]);
-						glTexCoord2f(1.0 , 1.0) ; glNormal3f(n[(t[i + 2] * 3) - 2] , n[(t[i + 2] * 3) - 1] , n[t[i + 2] * 3]) ; glVertex3f(c[(t[i] * 3) - 2] , c[(t[i] * 3) - 1] , c[t[i] * 3]);
+						glNormal3f(n[(t[i - 4] * 3) - 2] , n[(t[i - 4] * 3) - 1] , n[t[i - 4] * 3]) ; glVertex3f(c[(t[i - 6] * 3) - 2] , c[(t[i - 6] * 3) - 1] , c[t[i - 6] * 3]);
+						glNormal3f(n[(t[i - 1] * 3) - 2] , n[(t[i - 1] * 3) - 1] , n[t[i - 1] * 3]) ; glVertex3f(c[(t[i - 3] * 3) - 2] , c[(t[i - 3] * 3) - 1] , c[t[i - 3] * 3]);
+						glNormal3f(n[(t[i + 2] * 3) - 2] , n[(t[i + 2] * 3) - 1] , n[t[i + 2] * 3]) ; glVertex3f(c[(t[i] * 3) - 2] , c[(t[i] * 3) - 1] , c[t[i] * 3]);
 					glEnd();
 				}
 			}
 		}
-*/
+
+
+		void texturized () {
+			int i = 0;
+
+			if(q[0] > 0) {
+				for (i = 10 ; i <= q[0] ;  i+=12) {
+					glBindTexture(GL_TEXTURE_2D,textura.GLindex);
+					glBegin(GL_QUADS);
+						glNormal3f(n[(q[i - 7] * 3) - 2] , n[(q[i - 7] * 3) - 1] , n[q[i - 7] * 3]) ; glVertex3f(c[(q[i - 9] * 3) - 2] , c[(q[i - 9] * 3) - 1] , c[q[i - 9] * 3]);
+						glNormal3f(n[(q[i - 4] * 3) - 2] , n[(q[i - 4] * 3) - 1] , n[q[i - 4] * 3]) ; glVertex3f(c[(q[i - 6] * 3) - 2] , c[(q[i - 6] * 3) - 1] , c[q[i - 6] * 3]);
+						glNormal3f(n[(q[i - 1] * 3) - 2] , n[(q[i - 1] * 3) - 1] , n[q[i - 1] * 3]) ; glVertex3f(c[(q[i - 3] * 3) - 2] , c[(q[i - 3] * 3) - 1] , c[q[i - 3] * 3]);
+						glNormal3f(n[(q[i + 2] * 3) - 2] , n[(q[i + 2] * 3) - 1] , n[q[i + 2] * 3]) ; glVertex3f(c[(q[i] * 3) - 2] , c[(q[i] * 3) - 1] , c[q[i] * 3]);
+					glEnd();
+				}
+			}
+
+			if(t[0] > 0) {
+				for (i = 7 ; i <= t[0] ;  i+=9) {
+					glBindTexture(GL_TEXTURE_2D,textura.GLindex);
+					glBegin(GL_TRIANGLES);
+						glNormal3f(n[(t[i - 4] * 3) - 2] , n[(t[i - 4] * 3) - 1] , n[t[i - 4] * 3]) ; glVertex3f(c[(t[i - 6] * 3) - 2] , c[(t[i - 6] * 3) - 1] , c[t[i - 6] * 3]);
+						glNormal3f(n[(t[i - 1] * 3) - 2] , n[(t[i - 1] * 3) - 1] , n[t[i - 1] * 3]) ; glVertex3f(c[(t[i - 3] * 3) - 2] , c[(t[i - 3] * 3) - 1] , c[t[i - 3] * 3]);
+						glNormal3f(n[(t[i + 2] * 3) - 2] , n[(t[i + 2] * 3) - 1] , n[t[i + 2] * 3]) ; glVertex3f(c[(t[i] * 3) - 2] , c[(t[i] * 3) - 1] , c[t[i] * 3]);
+					glEnd();
+				}
+			}
+		}
+
 };
 
