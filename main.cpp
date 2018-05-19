28de26fff3e8d0910088c052fd71cadd2faa60da
naredokitsch <naredokitsch@utexas.edu>
2018-05-18 20:55:21 -0500

    starting ironman

--- main.cpp
+++ main.cpp
@@ -15,7 +15,27 @@ float pos_camX = 0, pos_camY = 0, pos_camZ = -5;
 float rot_iron = 0;
 int eye_camX = 0, eye_camY = 0.0, eye_camZ = 0;
 
-//CTexture cedro;
+char ironman_body_ [] = { 't','e','x','t','u','r','a','s','\\','b','o','d','y','.','t','g','a','\0' } ;
+char ironman_head_ [] = { 't','e','x','t','u','r','a','s','\\','h','e','a','d','.','t','g','a','\0' } ;
+
+float text_coord_body [] = {	0.25,4.0/6.0,0.75,4.0/6.0,0.75,0.5,0.25,0.5 ,
+										0.25,4.0/6.0,0.75,4.0/6.0,0.75,1,0.25,1 ,
+										0.25, 0.5, 0, 0.5, 0, 1.0/6.0, 0.25, 1.0/6.0,
+										0.75, 0.5, 1, 0.5, 1, 1.0/6.0, 0.75, 1.0/6.0,
+										0.75, 0.5, 0.25, 0.5, 0.25, 1.0/6.0, 0.75, 1.0/6.0, 
+										0.75, 0, 0.25, 0, 0.25, 1.0/6.0, 0.75, 1.0/6.0
+									};
+
+float text_coord_head [] = {	0.25,4.0/6.0,0.75,4.0/6.0,0.75,0.5,0.25,0.5 ,
+										0.25,4.0/6.0,0.75,4.0/6.0,0.75,1,0.25,1 ,
+										0.25, 0.5, 0, 0.5, 0, 1.0/6.0, 0.25, 1.0/6.0,
+										0.75, 0.5, 1, 0.5, 1, 1.0/6.0, 0.75, 1.0/6.0,
+										0.75, 0.5, 0.25, 0.5, 0.25, 1.0/6.0, 0.75, 1.0/6.0, 
+										0.75, 0, 0.25, 0, 0.25, 1.0/6.0, 0.75, 1.0/6.0
+									};
+
+CTexture ironman_body_texture;
+CTexture ironman_head_texture;
 
 
 void InitGL ( GLvoid )  {   // Inicializamos parametros
@@ -38,54 +58,61 @@ void InitGL ( GLvoid )  {   // Inicializamos parametros
 
    //SE CREAN LAS TEXTURAS
 
-	/*cedro.LoadTGA(cedro_);
-	cedro.BuildGLTexture();
-	cedro.ReleaseImage();*/
+	ironman_body_texture.LoadTGA(ironman_body_);
+	ironman_body_texture.BuildGLTexture();
+	ironman_body_texture.ReleaseImage();
+
+	ironman_head_texture.LoadTGA(ironman_head_);
+	ironman_head_texture.BuildGLTexture();
+	ironman_head_texture.ReleaseImage();
 }
 
 
-void cube(float width, float height, float depth) {
+void cube(float width, float height, float depth, float* text_coord, GLuint text_coord_index) {
 
+
+	glBindTexture(GL_TEXTURE_2D, text_coord_index);
 	glBegin(GL_QUADS); //bottom
-		glNormal3f(0,-1,0) ; glVertex3f( -(width/2.0) , -(height/2.0), depth/2.0);
-		glNormal3f(0,-1,0) ; glVertex3f( width/2.0 , -(height/2.0), depth/2.0);
-		glNormal3f(0,-1,0) ; glVertex3f( width/2.0 , -(height/2.0), -(depth/2.0));
-		glNormal3f(0,-1,0) ; glVertex3f( -(width/2.0) , -(height/2.0), -(depth/2.0));
+		glTexCoord2f(text_coord[0],text_coord[1]) ; glNormal3f(0,-1,0) ; glVertex3f( -(width/2.0) , -(height/2.0), depth/2.0);
+		glTexCoord2f(text_coord[2],text_coord[3]) ; glNormal3f(0,-1,0) ; glVertex3f( width/2.0 , -(height/2.0), depth/2.0);
+		glTexCoord2f(text_coord[4],text_coord[5]) ; glNormal3f(0,-1,0) ; glVertex3f( width/2.0 , -(height/2.0), -(depth/2.0));
+		glTexCoord2f(text_coord[6],text_coord[7]) ; glNormal3f(0,-1,0) ; glVertex3f( -(width/2.0) , -(height/2.0), -(depth/2.0));
 	glEnd();
 
+	
 	glBegin(GL_QUADS); //front
-		glNormal3f(0,0,1) ; glVertex3f( -(width/2.0) , -(height/2.0), depth/2.0);
-		glNormal3f(0,0,1) ; glVertex3f( width/2.0 , -(height/2.0), depth/2.0);
-		glNormal3f(0,0,1) ; glVertex3f( width/2.0 , height/2.0, depth/2.0);
-		glNormal3f(0,0,1) ; glVertex3f( -(width/2.0) , height/2.0, depth/2.0);
+		glTexCoord2f(text_coord[8],text_coord[9]) ; glNormal3f(0,0,1) ; glVertex3f( -(width/2.0) , -(height/2.0), depth/2.0);
+		glTexCoord2f(text_coord[10],text_coord[11]) ; glNormal3f(0,0,1) ; glVertex3f( width/2.0 , -(height/2.0), depth/2.0);
+		glTexCoord2f(text_coord[12],text_coord[13]) ; glNormal3f(0,0,1) ; glVertex3f( width/2.0 , height/2.0, depth/2.0);
+		glTexCoord2f(text_coord[14],text_coord[15]) ; glVertex3f( -(width/2.0) , height/2.0, depth/2.0);
 	glEnd();
 
 	glBegin(GL_QUADS); //left
-		glNormal3f(-1,0,0) ; glVertex3f( -(width/2.0) , -(height/2.0), depth/2.0);
-		glNormal3f(-1,0,0) ; glVertex3f(-(width/2.0) , -(height/2.0), -(depth/2.0));
-		glNormal3f(-1,0,0) ; glVertex3f(-(width/2.0) , height/2.0, -(depth/2.0));
-		glNormal3f(-1,0,0) ; glVertex3f(-(width/2.0) , height/2.0, depth/2.0);
+		glTexCoord2f(text_coord[16],text_coord[17]) ; glNormal3f(-1,0,0) ; glVertex3f( -(width/2.0) , -(height/2.0), depth/2.0);
+		glTexCoord2f(text_coord[18],text_coord[19]) ; glNormal3f(-1,0,0) ; glVertex3f(-(width/2.0) , -(height/2.0), -(depth/2.0));
+		glTexCoord2f(text_coord[20],text_coord[21]) ; glNormal3f(-1,0,0) ; glVertex3f(-(width/2.0) , height/2.0, -(depth/2.0));
+		glTexCoord2f(text_coord[22],text_coord[23]) ; glNormal3f(-1,0,0) ; glVertex3f(-(width/2.0) , height/2.0, depth/2.0);
 	glEnd();
 
 	glBegin(GL_QUADS); //right
-		glNormal3f(1,0,0) ; glVertex3f(width/2.0 , -(height/2.0), depth/2.0);
-		glNormal3f(1,0,0) ; glVertex3f(width/2.0 , -(height/2.0), -(depth/2.0));
-		glNormal3f(1,0,0) ; glVertex3f(width/2.0 , height/2.0, -(depth/2.0));
-		glNormal3f(1,0,0) ; glVertex3f(width/2.0 , height/2.0, depth/2.0);
+		glTexCoord2f(text_coord[24],text_coord[25]) ; glNormal3f(1,0,0) ; glVertex3f(width/2.0 , -(height/2.0), depth/2.0);
+		glTexCoord2f(text_coord[26],text_coord[27]) ; glNormal3f(1,0,0) ; glVertex3f(width/2.0 , -(height/2.0), -(depth/2.0));
+		glTexCoord2f(text_coord[28],text_coord[29]) ; glNormal3f(1,0,0) ; glVertex3f(width/2.0 , height/2.0, -(depth/2.0));
+		glTexCoord2f(text_coord[30],text_coord[31]) ; glNormal3f(1,0,0) ; glVertex3f(width/2.0 , height/2.0, depth/2.0);
 	glEnd();
 
 	glBegin(GL_QUADS); //back
-		glNormal3f(0,0,-1) ; glVertex3f( -(width/2.0) , -(height/2.0), -(depth/2.0));
-		glNormal3f(0,0,-1) ; glVertex3f( width/2.0 , -(height/2.0), -(depth/2.0));
-		glNormal3f(0,0,-1) ; glVertex3f(width/2.0 , height/2.0, -(depth/2.0));
-		glNormal3f(0,0,-1) ; glVertex3f( -(width/2.0) , height/2.0, -(depth/2.0));
+		glTexCoord2f(text_coord[32],text_coord[33]) ; glNormal3f(0,0,-1) ; glVertex3f( -(width/2.0) , -(height/2.0), -(depth/2.0));
+		glTexCoord2f(text_coord[34],text_coord[35]) ; glNormal3f(0,0,-1) ; glVertex3f( width/2.0 , -(height/2.0), -(depth/2.0));
+		glTexCoord2f(text_coord[36],text_coord[37]) ; glNormal3f(0,0,-1) ; glVertex3f(width/2.0 , height/2.0, -(depth/2.0));
+		glTexCoord2f(text_coord[38],text_coord[39]) ; glNormal3f(0,0,-1) ; glVertex3f( -(width/2.0) , height/2.0, -(depth/2.0));
 	glEnd();
 
 	glBegin(GL_QUADS); //top
-		glNormal3f(0,1,0) ; glVertex3f( -(width/2.0) , height/2.0, depth/2);
-		glNormal3f(0,1,0) ; glVertex3f( width/2.0 , height/2.0, depth/2);
-		glNormal3f(0,1,0) ; glVertex3f( width/2.0 , height/2.0, -(depth/2));
-		glNormal3f(0,1,0) ; glVertex3f( -(width/2.0) , height/2.0, -(depth/2));
+		glTexCoord2f(text_coord[40],text_coord[41]) ; glNormal3f(0,1,0) ; glVertex3f( -(width/2.0) , height/2.0, depth/2);
+		glTexCoord2f(text_coord[42],text_coord[43]) ; glNormal3f(0,1,0) ; glVertex3f( width/2.0 , height/2.0, depth/2);
+		glTexCoord2f(text_coord[44],text_coord[45]) ; glNormal3f(0,1,0) ; glVertex3f( width/2.0 , height/2.0, -(depth/2));
+		glTexCoord2f(text_coord[46],text_coord[48]) ; glNormal3f(0,1,0) ; glVertex3f( -(width/2.0) , height/2.0, -(depth/2));
 	glEnd();
 
 }
@@ -96,7 +123,6 @@ void display ( void )  { // Creamos la funcion donde se dibuja
 	
 	glLoadIdentity();
 
-	glPushMatrix();
 	glTranslatef(pos_camX, pos_camY, pos_camZ);
 		glRotatef(eye_camX, 1.0, 0.0, 0.0);
 		glRotatef(eye_camY, 0.0, 1.0, 0.0);
@@ -104,17 +130,58 @@ void display ( void )  { // Creamos la funcion donde se dibuja
 
 
 		glPushMatrix();//body
+			glPointSize(10); 
+
 			glTranslatef(0,6,0);
-			cube(8,12,4);//CUERPO
-			glTranslatef(0,6,0);
-			glRotatef(rot_iron, 0,1,0);//ROTACION DE CUELLO
-			glTranslatef(0,4,0);
-			cube(8,8,8);//CABEZA
-			glTranslatef(4,-6,0);
+			cube(8,12,4, text_coord_body, ironman_body_texture.GLindex);//CUERPO
+			glTranslatef(0,4,0);				//ALTURA HOMBROS
+			/*
+			glPushMatrix();					
+				glTranslatef(0,2,0);
+				glRotatef(rot_iron, 0,1,0);//ROTACION DE CUELLO
+				glTranslatef(0,4,0);
+				cube(8,8,8);					//CABEZA
+			glPopMatrix();
+			*/
+			/*
+			glTranslatef(4,0,0);				//HOMBRO DERECHO
+
+			glPushMatrix();
+				glRotatef(rot_iron, 1,0,0);
+				glTranslatef(2,-4,0);
+				cube(4,12,4);
+			glPopMatrix();
+
+			glTranslatef(-8,0,0);
+			//glBegin(GL_POINTS); glVertex3f(0,0,0); glEnd();
+
+			glPushMatrix();
+				glRotatef(rot_iron, 1,0,0);
+				glTranslatef(-2,-4,0);
+				cube(4,12,4);
+			glPopMatrix();*/
+		glPopMatrix();
+		/*
+		glPushMatrix();
+
+			glTranslatef(-2,0,0);
+
+			glPushMatrix();
+				glRotatef(rot_iron, 1,0,0);
+				glTranslatef(0,-6,0);
+				cube(4,12,4);
+			glPopMatrix();
+
+			glTranslatef(4,0,0);
+
+			glPushMatrix();
+				glRotatef(rot_iron, 1,0,0);
+				glTranslatef(0,-6,0);
+				cube(4,12,4);
+			glPopMatrix();
 
 		glPopMatrix();
-
-
+		*/
 
 	glutSwapBuffers ( );
 }
